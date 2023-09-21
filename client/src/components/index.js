2551728759fbdfd2b998e30db45f3dd4b1a9ec03
Renee Sen <rensen@Renees-Laptop.fios-router.home>
2023-09-21 11:03:57 -0400

    added all new files

---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import AIPicker from "./AIPicker";
+import Tab from "./Tab";
+export { 
+        CustomButton, 
+        FilePicker, 
+        ColorPicker,
+        AIPicker, 
+        Tab
+    };
